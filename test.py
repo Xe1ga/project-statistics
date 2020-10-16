97b3c22af82e31eac1fdab8947a48cfb1c6ee4be
Sofya <sofya.salmanova@playrix.com>
2020-10-16 17:55:06 +0200

    test script fixed

--- test.py
+++ test.py
@@ -1,8 +1,13 @@
 
+def test_func_2(count: int = 5):
+    for _ in range(count):
+        test_func('Привет')
+
+
 def test_func(text: str, count: int = 10):
     for _ in range(count):
         print(text)
 
 
 if __name__ == "__main__":
-    test_func('Привет')
+    test_func_2()
